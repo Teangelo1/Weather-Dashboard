154b7ab67b8b07124e491af3c604e10a295c822c
Teangelo Burks <burks.teangelo@gmail.com>
2021-01-14 13:26:56 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+
+
+//     var queryURL = "https//pi.openweathermap.org/data/2.5/forecast?zip=" + zipCode + "us&appid" + 8c25f6eea238e937dd0781aab2ba170
+     
+// $.ajax({
+//     url: ,
+//     method: "GET"
+// });
+
+
+
+
+
+
+// });
