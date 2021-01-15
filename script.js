d7cb05d77f95b41f60cf118e0c12b71d020a471c
Teangelo Burks <burks.teangelo@gmail.com>
2021-01-15 14:18:29 -0500

    Search button now returns data from API to console log

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
 
-//     var queryURL = "https//pi.openweathermap.org/data/2.5/forecast?zip=" + zipCode + "us&appid" + 8c25f6eea238e937dd0781aab2ba170
-     
-// $.ajax({
-//     url: ,
-//     method: "GET"
-// });
+        // Search Button to return results.
+    $(".btn").on("click", function() {
 
-
+        var cityName = $("#input").val().trim();
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=8c25f6eea238e937dd0781aab2ba1708";
 
 
 
+    // This Will Display the current weather conditions.
+        
 
-// });
+
+
+
+
+
+        $("ul").append("<li>" + cityName + "</li>")
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+    });
+
+    
+    
+
+})
+
+
+
+
+
+
+
+
+});
