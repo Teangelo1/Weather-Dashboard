fa9460116147d4cc6e4e945b90a12a7ea682828f
Teangelo Burks <burks.teangelo@gmail.com>
2021-01-16 21:50:13 -0500

    Made Changes to HTML id's/classes to respond correctly when calling on them in javascript

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
 
-     let savedInput = JSON.parse(localStorage.getItem("savedInput"));
-     if(savedInput === null){
-         savedInput = []
-     };
-     // Search Button to return results.
-    $(".btn").on("click", function () {
+    let savedInput = JSON.parse(localStorage.getItem("savedInput"));
+    if (savedInput === null) {
+        savedInput = ["Detroit"] // If User refreshes page, keep last saved city entered
+    };
+    function citySearch(cityName) {
 
-       
-        var cityName = $("#input").val().trim();
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=8c25f6eea238e937dd0781aab2ba1708";
-        $("ul").prepend("<li>" + cityName + "</li>");
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -26,55 +22,76 @@ $(document).ready(function () {
                 console.log(response);
 
                 // Displaying Current date/Temp/Hum/WindS
-               var currentDate = Date(response.current.dt);
+                var currentDate = Date(response.current.dt);
                 $(".namecity").text(cityName);
                 $(".currentdate").text(currentDate);
                 $(".icon").attr("src", "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + ".png");
-                $(".currenttemp").text("Temperature: " + response.current.temp);
+                $(".currenttemp").text("Temperature: " + Math.floor((response.current.temp - 273) * 1.8 + 32));
                 $(".currenthumidity").text("Humidity: " + response.current.humidity + "%");
                 $(".current-wind-speed").text("Wind Speed: " + response.current.wind_speed);
-                $(".current-uvindex").text(response.daily[0].uvi);
+                $(".current-uvindex").text("UV Index: " + response.daily[0].uvi);
 
-                //Checking date to determine uv index color
+                //Checking temp to determine uv index color
                 if (response.daily[0].uvi <= 3) {
                     $("#uv-Index").addClass("green");
-                   } else if (response.daily[0].uvi >= 3 && response.daily[0].uvi < 6 ) {
-                     $("#uv-Index").addClass("yellow");
-                   } else if (response.daily[0].uvi >= 6 && response.daily[0].uvi < 9) {
-                       $("#uv-Index").addClass("orange");
-                   } else  { 
-                       $("#uv-Index").addClass("red");
-                   } console.log(response.current.uvi);
-            
+                } else if (response.daily[0].uvi >= 3 && response.daily[0].uvi < 6) {
+                    $("#uv-Index").addClass("yellow");
+                } else if (response.daily[0].uvi >= 6 && response.daily[0].uvi < 9) {
+                    $("#uv-Index").addClass("orange");
+                } else {
+                    $("#uv-Index").addClass("red");
+                } console.log(response.current.uvi);
 
-                   // For loop for 5 Day Forecast
-               var days = response.daily;
-               //used empty function so that recent forecast is cleared once new forecast is displayed
-               $("#forecast").empty();
 
-               for(var i = 1; i < 6; i++) {
-                var date = new Date (days[i].dt);
-                var temp = days[i].temp.day;
-                var humidity = days[i].humidity;
-                var container = $("<div>").addClass("col-md-3 future-day");
-                
-                var dateContainer = $("<div>").text(date);
-                var tempContainer = $("<div>").text(temp);
-                var humidityContainer = $("<div>").text(humidity);
+                // For loop for 5 Day Forecast
+                var days = response.daily;
+                //used empty function so that recent forecast is cleared once new forecast is displayed
+                $("#forecast").empty();
 
-                container.append(dateContainer,tempContainer,humidityContainer);
-                $("#forecast").append(container);
-                } 
+                for (var i = 1; i < 6; i++) {
+                    var date = new Date(days[i].dt); // Date is not displaying correctly
+                    var temp = days[i].temp.day;
+                    var humidity = days[i].humidity;
+                    var container = $("<div>").addClass("col-md-3 future-day");
+
+                    var dateContainer = $("<div>").text(date);
+                    var tempContainer = $("<div>").text(temp);
+                    var humidityContainer = $("<div>").text(humidity);
+
+                    container.append(dateContainer, tempContainer, humidityContainer);
+                    $("#forecast").append(container);
+                }
                 savedInput.push(cityName);
 
-               window.localStorage.setItem("savedInput", JSON.stringify(savedInput));
+                localStorage.setItem("savedInput", JSON.stringify(savedInput));
             });
-            
-    
-});
-        
 
-                })
 
+        });
+
+    }
+
+    // Search Button to add results to list
+    $(".mainSearch").on("click", function () {
+
+        var cityName = $("#input").val().trim();
+        let saveButton = $("<button>")
+            .addClass("savedSearch btn")
+            .attr("id", cityName)
+            .html(cityName);
+        $(".recent-searches").prepend(saveButton);
+
+
+        citySearch(cityName);
+
+    })
+
+    $(".recent-searches").on("click", (e) => {
+        e.preventDefault();
+        citySearch(e.target.id);
+
+    })
+
+citySearch(savedInput[savedInput.length - 1]);
 });
 
